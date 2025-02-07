88117c868a230d51dbb90ab00dd36f7a56c1bf69
HectorLHG <hector.henriquez@thenotcompany.com>
2025-02-07 15:30:56 -0800

    Eliminacion de template vue + vite

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,8 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import PokemonList from './components/PokemonList.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-  <p>----------------------------------------------------------------------------------</p>
   <h1 class="title">App para buscar Pokemones</h1>
   <PokemonList />
 </template>
